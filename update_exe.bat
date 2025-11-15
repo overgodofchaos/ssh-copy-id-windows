10f726884571d460745d6baf20faa5f32b3a4914
overgodofchaos <vezonzzzzzz@gmail.com>
2025-11-15 17:07:16 +0300

    v11 fix

--- update_exe.bat
+++ update_exe.bat
@@ -1,4 +1,5 @@
 poetry run nuitka --onefile ssh-copy-id.py --output-filename=ssh-copy-id.exe
+makedir %~dp0executable\ssh-copy-id-windows\bin
 move /Y %~dp0ssh-copy-id.exe %~dp0executable\ssh-copy-id-windows\bin\ssh-copy-id.exe
 7z a -tzip %~dp0executable\ssh-copy-id-windows.zip %~dp0executable\ssh-copy-id-windows
 pause
